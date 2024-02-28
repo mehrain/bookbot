9c10d8f6595a4668ce8e5b8395af1e1634204907
mehrain <marijnrovers@live.nl>
2024-02-28 12:13:24 +0100

    Test with word counter

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 def main():
+    word_count = 0
     with open("books/frankenstein.txt") as book:
-        print(book.read())
+        book_string = book.read()
+        word_list = book_string.split()
+        for i in range(0, len(word_list)):
+            word_count += 1
+    print(word_count)
+        
+        
 main()
