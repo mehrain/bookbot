ffeb7411171593d0fd066e4dfd88551c541ded40
mehrain <marijnrovers@live.nl>
2024-02-29 17:00:19 +0100

    letter counter added

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    letters_counted = letter_counter(text)
     print(f"{num_words} words found in the document")
+    print(f"The following letters where counted {letters_counted}")
+
+     
+
 
 
 def get_num_words(text):
@@ -14,5 +19,19 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+
+def letter_counter(text):
+    words = text.split()
+    text = ''.join(words)
+    lower_text = text.lower()
+    letter_dict = {}
+
+    for letter in lower_text:
+        if letter in letter_dict: 
+            letter_dict[letter] += 1
+        else: 
+            letter_dict[letter] = 1
+    return letter_dict
+        
 
 main()
